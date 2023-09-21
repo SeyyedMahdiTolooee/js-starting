4078c020e0c5cdbb65880cbdf13a293403c45563
SeyyedMahdiTolooee <tolouiy@gmail.com>
2023-09-21 16:10:46 +0330

    process

--- dom model/script.js
+++ dom model/script.js
@@ -1,1 +1,50 @@
-'use strict'; 
+'use strict';
+const modal = document.querySelector('#modal');
+const overlay = document.querySelector('#overlay');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.getElementById('closeModal');
+// const btnCloseModal = document.querySelector('#closeModal');
+const openModal = function () {
+  console.log(`button clicked`);
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+//   console.log(`modal closed`);
+// };
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+  console.log(`modal closed`);
+}
+
+console.log(btnShowModal);
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+}
+// btnCloseModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+//   console.log(`modal  closed`);
+// });
+// (overlay || btnCloseModal).addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+//   console.log(`modal closed`);
+// });
+
+// ( overlay ||).addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+//   console.log(`modal closed`);
+// });
+
+// btnCloseModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+//   console.log(`modal  closed`);
+// });
