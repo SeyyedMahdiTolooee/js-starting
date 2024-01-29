36d5aaa70dd50c9fc6b670b4b5409c00bf505e93
SeyyedMahdiTolooee <tolouiy@gmail.com>
2024-01-29 08:24:16 +0330

    process

--- sabzlearn.js/index.js
+++ sabzlearn.js/index.js
@@ -41,5 +41,65 @@ average(8, 12, 34);
 console.log("line 41 says", Math.PI);
 console.log("line 42 says", Math.sqrt(483));
 
-const randomNumber = Math.floor(Math.random() * (600 - 149) + 149);
+const randomNumber = Math.floor(Math.random() * (99999 - 10000) + 10000);
 console.log(randomNumber);
+let num1 = 10;
+let num2 = ++num1;
+console.log("num1", num1);
+console.log("num2", num2);
+// for (let i = 0; i < 100; i++) {
+// 	if (i % 2 == 0) console.log(i);
+// }
+// let userNumber = 0;
+// let sum = 0;
+// let count = 0;
+// let avg = 0;
+// while (userNumber != -1) {
+// 	userNumber = Number(
+// 		prompt(
+// 			`enter your number \n now the sum is ${sum}\n and the average is ${Math.floor(
+// 				avg,
+// 			)}`,
+// 		),
+// 	);
+// 	if (userNumber != -1) {
+// 		count++;
+// 		sum += userNumber;
+// 		avg = sum / count;
+// 		console.log(
+// 			`the counter is on${count}\n your number is ${userNumber}\n now the sum is ${sum}\n and the average is ${Math.floor(
+// 				avg,
+// 			)}\n\n`,
+// 		);
+// 	}
+// }
+// let i = 0;
+// do {
+// 	i++;
+// 	console.log(i);
+// } while (i < 3);
+
+let firstNumber = Number(prompt("enter first number:", 0));
+let secondNumber = Number(prompt("enter second number:", 0));
+let power = 1;
+
+for (let i = 0; i < secondNumber; i++) {
+	power = power * firstNumber;
+	console.log(power);
+}
+console.log("the power is:", power);
+let j = 0;
+let power2 = 1;
+while (j < secondNumber) {
+	power2 = power2 * firstNumber;
+	console.log(power2);
+	j++;
+}
+console.log("the while power is", power2);
+let power3 = 1
+let k=0
+do {
+	k++
+	power3=power3*firstNumber
+} while (k < secondNumber);
+console.log('do while power is',power3)
